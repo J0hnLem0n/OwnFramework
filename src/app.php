1c0ba46175e5acf749e5644424ba7d886cc64f44
JohnLemon <nikolaevee@gmail.com>
2017-02-01 00:07:18 +0600

    add controllers

--- src/app.php
+++ src/app.php
@@ -1,8 +1,27 @@
 <?php
 use Symfony\Component\Routing;
+use \Symfony\Component\HttpFoundation\Response;
+
+class LeapYearController {
+    public function indexAction($year) {
+        if ($this->is_leap_year($year)) {
+            return new Response('Да это високосный год');
+        }
+        return new Response('Нет, это не високосный год');
+    }
+    private function is_leap_year($year = null) {
+        if (null === $year) {
+            $year = date('Y');
+        }
+        return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+    }
+}
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
-
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}',
+    array(
+        'year' => null,
+        '_controller' => 'LeapYearController::indexAction'
+    )
+));
 return $routes;
--- web/front.php
+++ web/front.php
@@ -6,6 +6,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -13,12 +14,14 @@ $routes = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $controller = $resolver->getController($request);
+    $argumens = $resolver->getArguments($request, $controller);
+    var_dump($argumens);
+    $response = call_user_func_array($controller, $argumens);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
