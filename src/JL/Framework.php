c841f5e0c799e3c51c27571fbb693343eb970db1
JohnLemon <nikolaevee@gmail.com>
2017-02-05 16:18:40 +0600

    unit tests

--- src/JL/Framework.php
+++ src/JL/Framework.php
@@ -3,15 +3,18 @@
 namespace JL;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+//для тестов
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 
 class Framework {
     protected $matcher;
     protected $resolver;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $resolver) {
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver) {
         $this->matcher = $matcher;
         $this->resolver = $resolver;
     }
